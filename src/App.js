d3ebf68c3a87762a555d57613aaa1de8f22a3e82
Anna A <anna.arikainen@gmail.com>
2022-08-07 16:57:45 +0300

    1.3: kurssitiedot step3

--- src/App.js
+++ src/App.js
@@ -1,61 +1,50 @@
-/* Refaktoroi sovelluksen koodi siten, että se koostuu kolmesta uudesta komponentista: Header, Content ja Total. 
-Kaikki data pidetään edelleen komponentissa App, joka välittää tarpeelliset tiedot kullekin komponentille props:ien avulla. 
-Header huolehtii kurssin nimen renderöimisestä, Content osista ja niiden tehtävämääristä ja Total tehtävien yhteismäärästä.
-
-const App = () => {
-  // const-määrittelyt
-
-  return (
-    <div>
-      <Header course={course} />
-      <Content ... />
-      <Total ... />
-    </div>
-  )
-}
-*/
+import React from "react"
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
+
+  //console.log(part1.name)
+  //console.log(part1.exercises)
 
   const Header = (props) => {
+    //console.log(props.course)
     return (
-      <div>
         <h1>
           {props.course}
         </h1>
-      </div>
     )
   }
 
   const Part = (props) => {
+    console.log(props.part1)
     return (
       <div>
         <p>
-          {props.part1} {props.exercises1}
-        </p>
-        <p>
-          {props.part2} {props.exercises2}
-        </p>
-        <p>
-          {props.part3} {props.exercises3}
+          {props.name} {props.exercises}
         </p>
       </div>
     )
   }
 
   const Content = (props) => {
+    console.log(props);
     return (
       <div>
-        <Part part1={props.part1} exercises1={props.exercises1} />
-        <Part part2={props.part2} exercises2={props.exercises2} />
-        <Part part3={props.part3} exercises3={props.exercises3} />
+        <Part name={props.part1.name} exercises={props.part1.exercises} />
+        <Part name={props.part2.name} exercises={props.part2.exercises} />
+        <Part name={props.part3.name} exercises={props.part3.exercises} />
       </div>
     )
   }
@@ -64,7 +53,7 @@ const App = () => {
     return (
       <div>
         <p>
-          Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}
+          Number of exercises {props.part1.exercises + props.part2.exercises + props.part3.exercises}
         </p>
       </div>
     )
@@ -72,9 +61,9 @@ const App = () => {
 
   return (
     <div>
-      <Header course={course} />
-      <Content part1={part1} part2={part2} part3={part3} exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
-      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
+      <Header course={course}/>
+      <Content part1={part1} part2={part2} part3={part3} exercises1={part1} exercises2={part2} exercises3={part3}/>
+      <Total part1={part1} part2={part2} part3={part3} />
     </div>
   )
 }
