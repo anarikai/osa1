37df40585339423c75a9e478777fb06a46f291b6
Anna A <anna.arikainen@gmail.com>
2022-08-14 15:13:20 +0300

    1.6: unicafe step1

--- unicafe/src/App.js
+++ unicafe/src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
 
-function App() {
+// nappi-komponentti
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
+
+// renderöidään vaihtoehdot ja niiden saamat äänimäärät omassa komponentissa
+const Value = ({good, neutral, bad}) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
     </div>
-  );
+  )
 }
 
-export default App;
+const App = () => {
+  // tallenna napit omaan tilaansa
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+
+const handleGoodClick = () => {
+  setGood(good + 1)
+}
+
+const handleNeutralClick = () => {
+  setNeutral(neutral + 1)
+}
+
+const handleBadClick = () => {
+  setBad(bad + 1)
+}
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button handleClick={handleGoodClick} text="good" />
+      <Button handleClick={handleNeutralClick} text="neutral" />
+      <Button handleClick={handleBadClick} text="bad" />
+      <h1>statistics</h1>
+      <Value good={good} neutral={neutral} bad={bad} />
+    </div>
+  )
+}
+
+export default App
