7d537360a5f869885f0a44ffbe6c14874bb4256b
Anna A <anna.arikainen@gmail.com>
2022-08-14 21:28:25 +0300

    1.7: unicafe step2

--- unicafe/src/App.js
+++ unicafe/src/App.js
@@ -8,12 +8,15 @@ const Button = ({ handleClick, text }) => (
 )
 
 // renderöidään vaihtoehdot ja niiden saamat äänimäärät omassa komponentissa
-const Value = ({good, neutral, bad}) => {
+const Value = ({good, neutral, bad, all, average, positive}) => {
   return (
     <div>
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {all}</p>
+      <p>average {average}</p>
+      <p>positive {positive} %</p>
     </div>
   )
 }
@@ -23,18 +26,26 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
+  const [average, setAverage] = useState(0)
 
-
+//for each good +1 - (forEach bad +1) / all 
 const handleGoodClick = () => {
   setGood(good + 1)
+  setAll(all + 1)
+  setAverage(average + 1)
 }
 
 const handleNeutralClick = () => {
   setNeutral(neutral + 1)
+  setAll(all + 1)
+  setAverage(average + 0)
 }
 
 const handleBadClick = () => {
   setBad(bad + 1)
+  setAll(all + 1)
+  setAverage(average - 1)
 }
 
   return (
@@ -44,7 +55,7 @@ const handleBadClick = () => {
       <Button handleClick={handleNeutralClick} text="neutral" />
       <Button handleClick={handleBadClick} text="bad" />
       <h1>statistics</h1>
-      <Value good={good} neutral={neutral} bad={bad} />
+      <Value good={good} neutral={neutral} bad={bad}  all={all} average={average/all} positive={good/all*100} />
     </div>
   )
 }
